78f3577b812f5b05a873a8bbf395de8bf46fb027
M99nie <a615hi03112016@gmail.com>
2024-04-09 09:47:04 +0600

    Парсер начал парсить

---
+++ Lab_rab1/Parser.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup # импортируем библиотеку BeautifulSoup
+import requests # импортируем библиотеку requests
+def parse():
+    url = 'https://auto.drom.ru/'  # передаем необходимы URL адрес
+    page = requests.get(url)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    print(page.status_code)  # смотрим ответ
+    soup = BeautifulSoup(page.text, "html.parser")  # передаем страницу в bs4
+
+    block = soup.findAll('a', class_='css-4zflqt e1huvdhj1')  # находим  контейнер с нужным классом
+    for car in block:  # проходим циклом по содержимому контейнера
+        name = car.find('div', class_='css-16kqa8y e3f4v4l2')
+        nameCar = name.text
+        print(nameCar)
+        price = car.find('div', class_='css-1dv8s3l eyvqki91')
+        priceCar = price.text
+        print(priceCar)
+
+
+parse()
