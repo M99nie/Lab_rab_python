70634cc2e94c013000367405208ab201d1cd7594
M99nie <a615hi03112016@gmail.com>
2024-05-06 12:37:22 +0600

    Добавлен вывод результатов в текстовый файл

--- Lab_rab1/Parser.py
+++ Lab_rab1/Parser.py
@@ -7,13 +7,17 @@ def parse():
     soup = BeautifulSoup(page.text, "html.parser")  # передаем страницу в bs4
 
     block = soup.findAll('a', class_='css-4zflqt e1huvdhj1')  # находим  контейнер с нужным классом
+    file = open("cars.txt", 'a')
     for car in block:  # проходим циклом по содержимому контейнера
         name = car.find('div', class_='css-16kqa8y e3f4v4l2')
         nameCar = name.text
         print(nameCar)
         price = car.find('div', class_='css-1dv8s3l eyvqki91')
-        priceCar = price.text
+        priceCar = str(price.text)
         print(priceCar)
+        file.write(nameCar+'\n')
+        file.write(priceCar+ "\n")
 
+    file.close()
 
 parse()
